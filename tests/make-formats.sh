53b0d49ff7fcc24847581c70e0e38dc72cb4c49a
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2008-06-09 00:11:26 +0200

    Added simple tests for Mongolian and Monotonic Greek.

---
+++ tests/make-formats.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+source setuptests
+
+xetex -jobname xelatex -ini \*unicode-letters \\input latex.ltx
+xetex -jobname xetex -ini \*unicode-letters \\input plain \\dump
+pdftex -jobname pdflatex -ini \\pdfoutput1 \\input latex.ltx
