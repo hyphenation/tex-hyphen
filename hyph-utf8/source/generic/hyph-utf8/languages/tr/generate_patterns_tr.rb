f7895df50b862f93af23ab4f08cbcf16249a9ac2
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2008-06-25 19:49:06 +0200

    a new script for generating modern turkish hyphenation patterns

---
+++ hyph-utf8/source/generic/hyph-utf8/languages/tr/generate_patterns_tr.rb
@@ -0,0 +1,99 @@
+#!/usr/bin/env ruby
+#
+# This script generates hyphenation patterns for modern Turkish
+#
+# "Algorithm" originally developed by:
+#
+# /****************************************************************************
+#  *                                                                          *
+#  *          turk_hyf.c -- a program to produce PatGen-like hyphenation      *
+#  *                        patterns for the Turkish Language.                *
+#  *                                                                          *
+#  *          Copyright 1987, by Pierre A. MacKay.                            *
+#  *                                                                          *
+#  *          Humanities and Arts Computing Center                            *
+#  *          Mail-Stop DW-10                                                 *
+#  *          University of Washington                                        *
+#  *          Seattle, Washington 98105                                       *
+#  *                                                                          *
+#  ****************************************************************************/
+#
+# Script written in June 2008 by Mojca Miklavec
+# as part of "Unicode Hyphenation Patterns" project
+#
+# Thanks to previous work, modifications and suggestions by:
+# - H. Turgut Uyar <uyar at itu.edu.tr>
+# - S. Ekin Kocabas <kocabas at stanford.edu>
+
+# TODO: add a function to generate patterns for Ottoman Turkish
+
+# open file for writing the patterns
+$tr = File.new("hyph-tr.tex", "w")
+
+# write comments into the file
+def add_comment(str)
+	$tr.puts "% " + str.gsub(/\n/, "\n% ").gsub(/% \n/, "%\n")
+end
+
+# define a class of vowels and consonants
+vowels = %w{a e ı i o ö u ü}
+consonants = %w{b c ç d f g ğ h j k l m n p r s ş t v y z}
+
+# start the file
+add_comment(
+	"hyph-tr.tex\n\n" +
+	"File auto-generated from generate_patterns_tr.rb that is part of hyph-utf8\n\n" +
+	"Licence:\n" +
+	"- Free enough for Debian & TeX Live or any other distributor\n" +
+	"- If you are reading this and have some suggestion about what to put here, please advise\n" +
+	"- I (Mojca) would prefer to say 'public domain', but don't know what it means for the original authors\n\n" +
+	"Credits:\n" +
+	"- algorithm developed by P. A. MacKay for the Ottoman Texts Project in 1987\n" +
+	"- rules adapted for modern Turkish by H. Turgut Uyar <uyar at itu.edu.tr>\n" +
+	"- initiative to improve Turkish patterns by S. Ekin Kocabas <kocabas at stanford.edu>\n" +
+	"- script written by Mojca Miklavec <mojca.miklavec.lists at gmail.com> in June 2008\n\n" +
+	"Modifications:\n" +
+	"- adapted for the use on modern UTF-8 TeX engines\n" +
+	"- UTF-8 patterns are used\n" +
+	"- only letters for Modern Turkish\n" +
+	"- support for Ottoman Turkish is now provided separately under 'ota' (not 'tr')\n\n" +
+	"Notes:\n" +
+	"- you need to use loadhyph-tr.tex, please do not try to put \\catcode-s & \\lccode-s here\n"
+)
+$tr.puts '\patterns{'
+
+# rules for hyphenation
+add_comment("prohibit hyphen before vowels, allow after")
+vowels.each do |vowel|
+	$tr.puts "2#{vowel}1"
+end
+
+add_comment("allow hyphen either side of consonants")
+consonants.each do |cons|
+	$tr.puts "1#{cons}1"
+end
+
+add_comment("prevent e-cek at end of word")
+$tr.puts "2e2cek."
+
+add_comment("prohibit hyphen before pair of consonants\nmany pairs generated here are impossible anyway")
+consonants.each do |c1|
+	consonants.each do |c2|
+		$tr.puts "2#{c1}#{c2}"
+	end
+end
+
+add_comment("allow hyphen between vowels, but not after second vowel of pair\nseveral phonetically impossible pairs here")
+vowels.each do |v1|
+	vowels.each do |v2|
+		$tr.puts "#{v1}3#{v2}2"
+	end
+end
+
+add_comment("a couple of consonant-clusters")
+$tr.puts 'tu4r4k'
+$tr.puts 'm1t4rak'
+
+# end the file
+$tr.puts '}'
+$tr.close
