16773d466ab001a554e8d66085e0bf5fac811ff1
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2009-05-19 00:37:25 +0200

    Script for making CTAN zip.

---
+++ tools/make_CTAN_zip.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+TMPDIR=`mktemp -d /tmp/hyphXXXXXX`
+cd $TMPDIR
+svn co svn://tug.org/texhyphen/trunk/hyph-utf8
+find . -name .svn -exec rm -rf {} \;
+zip -ry hyph-utf8.zip hyph-utf8
+rm -rf hyph-utf8
+echo "$TMPDIR/hyph-utf8.zip ready to be shipped to CTAN."
