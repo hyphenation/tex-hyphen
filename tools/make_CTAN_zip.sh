a56c6fc5a1aa8765bd5c4579ced2a45c2bc91a6a
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2009-05-19 01:33:08 +0200

    CTAN zip utility now takes tags/CTAN in account, not trunk.

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -2,7 +2,7 @@
 
 TMPDIR=`mktemp -d /tmp/hyphXXXXXX`
 cd $TMPDIR
-svn co svn://tug.org/texhyphen/trunk/hyph-utf8
+svn co svn://tug.org/texhyphen/tags/CTAN/hyph-utf8
 find . -name .svn -exec rm -rf {} \;
 zip -ry hyph-utf8.zip hyph-utf8
 rm -rf hyph-utf8
