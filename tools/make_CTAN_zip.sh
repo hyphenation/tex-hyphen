cba91ddb724dcb59fd0729ec68b15eec6414889c
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2009-05-30 18:55:17 +0200

    add symbolic link to README

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -2,8 +2,14 @@
 
 TMPDIR=`mktemp -d /tmp/hyphXXXXXX`
 cd $TMPDIR
+# make sure to use 
+#     use-commit-times = yes
+# in ~/.subversion/config
 svn co svn://tug.org/texhyphen/tags/CTAN/hyph-utf8
 find . -name .svn -exec rm -rf {} \;
+cd hyph-utf8
+ln -s doc/generic/hyph-utf8/README .
+cd ..
 zip -ry hyph-utf8.zip hyph-utf8
 rm -rf hyph-utf8
 echo "$TMPDIR/hyph-utf8.zip ready to be shipped to CTAN."
