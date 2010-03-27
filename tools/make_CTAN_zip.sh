b41ddd3e5f77a3a53b96b50c5d942f68fafbbd83
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2010-03-27 21:52:53 +0100

    take trunk (for a moment) and remove one folder from zip

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -5,11 +5,11 @@ cd $TMPDIR
 # make sure to use 
 #     use-commit-times = yes
 # in ~/.subversion/config
-svn co svn://tug.org/texhyphen/tags/CTAN/hyph-utf8
+svn co svn://tug.org/texhyphen/trunk/hyph-utf8
 find . -name .svn -exec rm -rf {} \;
 cd hyph-utf8
 ln -s doc/generic/hyph-utf8/README .
+zip -ry ../hyph-utf8.zip *
 cd ..
-zip -ry hyph-utf8.zip hyph-utf8
 rm -rf hyph-utf8
 echo "$TMPDIR/hyph-utf8.zip ready to be shipped to CTAN."
