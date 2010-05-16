69b5a056b6b1c2a9f936fe3cbdaa2abf9f5dd601
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2010-05-16 23:42:19 +0200

    auto-generated patterns for Hyphenator.js

---
+++ collaboration/hyphenator/repo/eu.js
@@ -0,0 +1,12 @@
+﻿Hyphenator.languages.eu = {
+	leftmin : 2,
+	rightmin : 2,
+	shortestPattern : 2,
+	longestPattern : 4,
+	specialChars : 'ñ',
+	patterns : {
+		3 : '1ba1be1bo1bi1bu1ca1ce1co1ci1cu1da1de1do1di1du1fa1fe1fo1fi1fu1ga1ge1go1gi1gu1ja1je1jo1ji1ju1ka1ke1ko1ki1ku1la1le1lo1li1lu1ma1me1mo1mi1mu1na1ne1no1zu1nu1ña1ñe1ño1ñi1ñu1pa1pe1po1pi1pu1qa1qe1qo1qi1qu1ra1re1ro1ri1ru1sa1se1so1si1su1ta1te1to1ti1tu1va1ve1vo1vi1vu1wa1we1wo1wi1wu1xa1xe1xo1xi1xu1ya1ye1yo1yi1yu1za1ze1zo1zi1ni'
+		5 : '1l2la1l2le1l2lo1l2li1l2lu1r2ra1r2re1r2ro1r2ri1r2ru1t2sa1t2se1t2so1t2si1t2su1t2xa1t2xe1t2xo1t2xi1t2xu1t2za1t2ze1t2zo1t2zi1t2zu1b2la1b2le1b2lo1b2li1b2lu1b2ra1b2re1b2ro1b2ri1b2ru1d2ra1d2re1d2ro1d2ri1d2ru1f2la1f2le1f2lo1f2li1f2lu1f2ra1f2re1f2ro1f2ri1f2ru1g2la1g2le1g2lo1g2li1g2lu1g2ra1g2re1g2ro1g2ri1g2ru1k2la1k2le1k2lo1k2li1t2ru1k2ra1k2re1k2ro1k2ri1k2ru1p2la1p2le1p2lo1p2li1p2lu1p2ra1p2re1p2ro1p2ri1p2ru1t2ra1t2re1t2ro1t2ri1k2lu'
+		6 : 'su2b2rsu2b2l'
+	}
+};
---
+++ collaboration/hyphenator/repo/it.js
@@ -0,0 +1,18 @@
+﻿Hyphenator.languages.it = {
+	leftmin : 2,
+	rightmin : 2,
+	shortestPattern : 1,
+	longestPattern : 8,
+	specialChars : '’',
+	patterns : {
+		2 : '1z1r1q1p1n1m1t1l1k1j1h1g1f1d1c1b1v1w1x'
+		3 : 'p2l2xf2xc2xb2xm2w’2w.w2h2xp2v.2vvv2rv2l2’22vc2xt2t.t2z2tw2tv2ttt2se2wt2r2tp2tn2tm2xw2bb2bc2bd2bf2bm2bn2bp2bs2bt2bvb2lb2r2b.2b’2x.2cb2cc2cd2cf2ck2cm2cn2cq2cs2ct2czt2lc2ht2h2tg2tfc2lc2r2c.2c’.c22x’2db2dd2rx2dl2dm2dn2dpd2r2ds2dt2dv2dw2d.2d’.d2y1i2fb2fg2ff2fnf2lf2r2fs2ft2f.2f’.z22gb2gd2gf2ggg2hg2l2gmg2n2gpg2r2gs2gt2gv2gw2gz2td2g.2g’2zb2hb2hd2hh2tch2l2hm2hn2hr2hv2h.2h’2zd2j.2j’2zl2kg2kfk2h2kkk2l2kmk2r2ks2kt2k.2k’2zn2lb2lc2ld2tb2lgl2h2lk2ll2lm2ln2lp2lq2lr2ls2lt2lv2lw2lz2l.2zp4s.2zt2mb2mc2mf2ml2mm2mn2mp2mq2mr2ms2mt2mv2mw2m.2m’2zs2nb2nc2nd2nf2ng2nk2nl2nm2nn2np2nq2nr2ns2sz2nt2nv2nz1s22r’2n.2n’2zv2pdp2h2xh2pn2r.2ppp2r2ps2rz2pt2pz2p.2p’2zz2qq2q.2q’2z.2rb2rc2rd2rfr2h2rg2rk2rl2rm2rn2rp2rq2rr2rs2rt2rw2rv2dg'
+		4 : '2t’.2w1y2z’’wa2ra1ia2shm2sh.2sh’2s3sa1iey1ou2stb2stc2std2stf2stg2stm2stn2stp2sts2stt2stv.p2s2l’’4s’.4s’’2l’.tz2s2v’’2t’’2chnch2r2chb2chho1iuo1ioo1ieo1iae1iu2v’.2at.a1ioa1yaa1uoa1iu2tzk2z’.'
+		5 : 't2t3s.bio12gh2ts4s3mn2g3n3p2ne2l3f2.pre1'
+		6 : '.anti13t2schhi3p2n.free32s3p2n2nheit3p2sic.opto1.para1r2t2s3.a3p2n'
+		7 : 'n2s3fer.e2x1eu.wel2t1.wa2g3n.su2b3r.narco1.ca4p3s'
+		8 : '.orto3p2.su2b3lu.contro1.poli3p2.li3p2sa.sha2re3.fran2k3'
+		9 : '.tran2s3t.tran2s3c.tran2s3r.circu2m1.tran2s3p.tran2s3n.re1i2scr.tran2s3l.tran2s3d.anti3m2n'
+		10 : '.di2s3cine'
+	}
+};
