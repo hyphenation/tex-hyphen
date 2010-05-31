e88bf872cf3d1cee6e4debbbf02cf053d86e4831
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2010-06-01 01:32:20 +0200

    fix converted patterns and adapt the loaders

--- tests/make-formats.sh
+++ tests/make-formats.sh
@@ -2,6 +2,8 @@
 
 source setuptests
 
-xetex -jobname xelatex -ini \*unicode-letters \\input latex.ltx
-xetex -jobname xetex -ini \*unicode-letters \\input plain \\dump
-pdftex -jobname pdflatex -ini \\pdfoutput1 \\input latex.ltx
+xetex  -jobname=xelatex  -ini \*unicode-letters \\input latex.ltx
+xetex  -jobname=xetex    -ini \*unicode-letters \\input plain \\dump
+pdftex -jobname=pdflatex -ini \\pdfoutput1 \\input latex.ltx
+# TODO: this might need to be fixed?
+ptex   -jobname=platex   -ini -progname=platex platex.ini
