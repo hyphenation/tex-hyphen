176a3a7485697ac92e517bd493fbc519c75558cc
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2010-10-26 02:39:05 +0200

    add original TeX patterns for hyphenator; plus some trivial fixes

---
+++ webpage/hyphenator/patterns/it.js
@@ -0,0 +1,18 @@
+﻿Hyphenator.languages.it = {
+	leftmin : 2,
+	rightmin : 2,
+	shortestPattern : 1,
+	longestPattern : 8,
+	specialChars : '’',
+	patterns : {
+		2 : '1z1r1q1v1p1n1m1l1k1j1h1g1f1d1w1c1b1t1x'
+		3 : '2n’2xt2xp2xm2xh2xf2xc2xbt2z2w’2w.t2st2r2’22tp2tn2tmt2lt2h2tg2tf2tde2w2tc2tb2x.4s.2x’2bb2bc2bd2bf2bm2bn2bp2bs2bt2bvb2lb2r2b.2b’2tt2cb2cc2cd2cf2ck2cm2cn2cq2cs2ct2cz2szc2h2t.w2hy1i2rt2r’2r.c2lc2r2c.2c’.c2.z22db2dd2dg2dl2dm2dn2dpd2r2ds2dt2dv2dw2d.2d’.d22zb2fb2fg2ff2fnf2lf2r2fs2ft2f.2f’2zd2gb2gd2gf2ggg2hg2l2gmg2n2gpg2r2gs2gt2gv2gw2gz2rz2g.2g’2zl2hb2hd2hh2rwh2l2hm2hn2hr2hv2h.2h’2zn2j.2j’2zp2kg2kfk2h2kkk2l2kmk2r2ks2kt2k.2k’2zt2lb2lc2ld2v.2lgl2hl2j2lk2ll2lm2ln2lp2lq2lr2ls2lt2lv2lw2lz2l.2rx2rv2zs2mb2mc2mf2ml2mm2mn2mp2mq2mr2ms2mt2mv2mw2m.2m’2zv2nb2nc2nd2nf2ng2nk2nl2nm2nn2np2nq2nr2ns2vv2nt2nv2nzv2rv2l2n.2xw2zz2pdp2hp2l2pn2vc2ppp2r2ps2z.2pt2pz2p.2p’2tv2qq2q.2q’2tw2rb2rc2rd2rfr2h2rg2rk2rl2rm2rn2rp2rq2rr2rs1s2'
+		4 : '2t’’2l’’2l’.2v’.2v’’2chnch2r2chb2shm2sh.2sh’2s3s2t’.2ch.2stb2stc2std2stf2stg2stm2stn2stp2sts2stt2stv2chho1iu4s’.4s’’o1ioo1ieo1iae1iu2at.a1yaa1uoa1iua1ioa1iea1iawa2r2w1y.p2sy1ou2tzk2z’.2z’’tz2s'
+		5 : 't2t3s.pre1.bio1s4s3m2ch’.2gh2t3p2nen2g3n2l3f2'
+		6 : '.anti12nheithi3p2n3p2sic2ch’’.r2t2s32s3p2n.free3.opto1.para13t2sch.a3p2n'
+		7 : '.narco1n2s3fer.wel2t1.ca4p3s.wa2g3n.su2b3r.e2x1eu'
+		8 : '.fran2k3.poli3p2.su2b3lu.orto3p2.sha2re3.contro1.li3p2sa'
+		9 : '.tran2s3p.tran2s3t.re1i2scr.tran2s3l.tran2s3c.circu2m1.tran2s3n.tran2s3r.anti3m2n.tran2s3d'
+		10 : '.di2s3cine'
+	}
+};
