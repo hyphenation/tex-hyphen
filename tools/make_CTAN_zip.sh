2c56115282be0b09bfedd10cd48bba61ae25a971
Mojca Miklavec <mojca.miklavec.lists@gmail.com>
2014-05-06 23:43:21 +0200

    add one extra top level dir on CTAN's request

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -9,7 +9,7 @@ svn co svn://tug.org/texhyphen/trunk/hyph-utf8
 find . -name .svn -exec rm -rf {} \;
 cd hyph-utf8
 ln -s doc/generic/hyph-utf8/README .
-zip -ry ../hyph-utf8.zip *
 cd ..
+zip -ry hyph-utf8.zip hyph-utf8
 rm -rf hyph-utf8
 echo "$TMPDIR/hyph-utf8.zip ready to be shipped to CTAN."
