e851d94bd2f8de10de08b6ce1cfa2ed73672f937
Arthur Reutenauer <arthur.reutenauer@normalesup.org>
2015-05-09 17:15:35 +0100

    Written a more reasonable top-level README.

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -8,7 +8,6 @@ cd $TMPDIR
 svn co svn://tug.org/texhyphen/trunk/hyph-utf8
 find . -name .svn -exec rm -rf {} \;
 cd hyph-utf8
-ln -s doc/generic/hyph-utf8/README .
 ln -s doc/generic/hyph-utf8/CHANGES .
 cd ..
 zip -ry hyph-utf8.zip hyph-utf8
