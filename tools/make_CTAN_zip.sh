c54d818c1987bc09cebfd37e5482b24ec2fccb8c
Arthur Reutenauer <arthur@reutenauer.eu>
2018-08-25 18:14:42 +0200

    Change to make_CTAN_zip to make Travis able to run it.

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -4,7 +4,8 @@ NAME=hyph-utf8
 TMPDIR=`mktemp -d /tmp/hyphXXXXXX`
 filename="$TMPDIR/$NAME.zip"
 
-pushd `dirname $0`/..
+oldwd=`pwd`
+cd `dirname $0`/..
 git archive --format=zip --prefix=$NAME/ --output="$filename" master:$NAME
 echo "$filename ready to be shipped to CTAN."
-popd
+cd $oldwd
