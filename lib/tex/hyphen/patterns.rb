0384a941a9cebddfaca5bff4c18076b8aaba624c
Arthur Reutenauer <arthur@reutenauer.eu>
2018-09-02 00:50:17 +0200

    Improved class and instance variable names in TeX::Hyphen::Patterns

--- lib/tex/hyphen/patterns.rb
+++ lib/tex/hyphen/patterns.rb
@@ -1,10 +1,13 @@
 module TeX
   module Hyphen
     class Patterns
-      @@texfiles = File.expand_path('../../../../hyph-utf8/tex/generic/hyph-utf8/patterns/tex', __FILE__)
+      @@topdir = File.expand_path('../../../../hyph-utf8/tex/generic/hyph-utf8/patterns', __FILE__)
       def initialize
-        @texfiles = Dir.glob(File.join(@@texfiles, 'hyph-*.tex')).map do |texfile|
+        @texfiles = Dir.glob(File.join(@@topdir, 'tex', 'hyph-*.tex')).map do |texfile|
           texfile.gsub /^.*\//, ''
+        end
+        @txtfiles = Dir.glob(File.join(@@topdir, 'txt', 'hyph-*.pat.txt')).map do |txtfile|
+          txtfile.gsub /^.*\/hyph-(.*)\.pat\.txt/, ''
         end
       end
     end
--- spec/tex/hyphen/patterns_spec.rb
+++ spec/tex/hyphen/patterns_spec.rb
@@ -3,12 +3,17 @@ require 'spec_helper'
 describe TeX::Hyphen::Patterns do
   describe '.new' do
     it "sets the path to the pattern files" do
-      expect(TeX::Hyphen::Patterns.class_variable_get :@@texfiles).to eq File.expand_path('../../../../hyph-utf8/tex/generic/hyph-utf8/patterns/tex', __FILE__)
+      expect(TeX::Hyphen::Patterns.class_variable_get :@@topdir).to eq File.expand_path('../../../../hyph-utf8/tex/generic/hyph-utf8/patterns', __FILE__)
     end
 
     it "loads all names of the TeX files" do
       patterns = TeX::Hyphen::Patterns.new
       expect(patterns.instance_variable_get(:@texfiles).count).to eq 79
+    end
+
+    it "loads all names of the text pattern files" do
+      patterns = TeX::Hyphen::Patterns.new
+      expect(patterns.instance_variable_get(:@txtfiles).count).to eq 77
     end
   end
 end
