aecf84418951ca285005ecd403e337919b4a55fb
Arthur Reutenauer <arthur.reutenauer@klarna.com>
2018-09-24 14:48:55 +0200

    Fixed whitespace.

--- lib/tex/hyphen/language.rb
+++ lib/tex/hyphen/language.rb
@@ -7,23 +7,23 @@ require_relative '../../../hyph-utf8/source/generic/hyph-utf8/languages'
 require_relative '../../../hyph-utf8/source/generic/hyph-utf8/language-data'
 
 module PATH
-	ROOT = File.expand_path('../../../..', __FILE__)
+  ROOT = File.expand_path('../../../..', __FILE__)
 
-	TeXROOT = File.join(ROOT, 'hyph-utf8')
-	TeX_GENERIC = File.join(TeXROOT, 'tex', 'generic')
-	TXT = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'txt')
-	TEX = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'tex')
-	PTEX = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'ptex')
-	QUOTE = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'quote')
+  TeXROOT = File.join(ROOT, 'hyph-utf8')
+  TeX_GENERIC = File.join(TeXROOT, 'tex', 'generic')
+  TXT = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'txt')
+  TEX = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'tex')
+  PTEX = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'ptex')
+  QUOTE = File.join(TeX_GENERIC, 'hyph-utf8', 'patterns', 'quote')
 
   SUPPORT = File.join(TeXROOT, '%s', 'generic', 'hyph-utf8', 'languages', '*')
 
   HYPHU8 = File.join('tex', 'generic', 'hyph-utf8')
 
-	TL = File.join(ROOT, 'TL')
-	LANGUAGE_DAT = File.join(PATH::TL, 'texmf-dist', 'tex', 'generic', 'config')
-	# hyphen-foo.tlpsrc for TeX Live
-	TLPSRC = File.join(PATH::TL, 'tlpkg', 'tlpsrc')
+  TL = File.join(ROOT, 'TL')
+  LANGUAGE_DAT = File.join(PATH::TL, 'texmf-dist', 'tex', 'generic', 'config')
+  # hyphen-foo.tlpsrc for TeX Live
+  TLPSRC = File.join(PATH::TL, 'tlpkg', 'tlpsrc')
 end
 
 
