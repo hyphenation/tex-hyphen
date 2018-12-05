9d40534c5cccb9b9697845ef4b1eeb127adb894f
Arthur Reutenauer <arthur.reutenauer@klarna.com>
2018-12-05 15:35:48 +0100

    ... and yet more stuff around.

--- hyph-utf8/source/generic/hyph-utf8/generate-tl-files.rb
+++ hyph-utf8/source/generic/hyph-utf8/generate-tl-files.rb
@@ -4,9 +4,13 @@
 # this file auto-generates tlpsrc files for hyphenation patterns - to be improved
 
 require_relative 'lib/tex/hyphen/language.rb'
+require_relative 'lib/tex/hyphen/texlive/package.rb'
+require_relative 'lib/tex/hyphen/texlive.rb'
+require_relative 'lib/tex/hyphen/texlive/loader.rb'
 include TeX::Hyphen
 include Language::TeXLive
 include TeXLive
+include Loader
 
 #--------#
 # TLPSRC #
