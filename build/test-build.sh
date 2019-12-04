417e07c9cf54368f1362fbafcaeb0376d619b8ea
Arthur Reutenauer <arthur.reutenauer@uniqode.se>
2019-12-04 17:59:40 +0100

    Fine-tuning build script.

--- build/test-build.sh
+++ build/test-build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 kubectl create -f pod.yaml
-sleep 5
+while test z = z"`kubectl get pod build | grep -F Running`"; do sleep 1; done
+
 kubectl exec build -- git clone --branch=build https://github.com/hyphenation/tex-hyphen
 kubectl exec build -- sh tex-hyphen/build/build.sh
