4639fca6bd755fa7e981ebee4d887c65de4ce773
Arthur Rosendahl <arthur.rosendahl@freespee.com>
2021-03-22 19:18:27 +0100

    Trying to make CTAN upload safer.

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -4,6 +4,10 @@ NAME=hyph-utf8
 TMPDIR=`mktemp -d /tmp/hyphXXXXXX`
 filename="$TMPDIR/$NAME.zip"
 
+if [ ! -z "$(git status -s)" ]; then
+  echo 'The repository is dirty; I won’t do anything.  Please clean up first.'
+  exit 42
+fi
 oldwd=`pwd`
 cd `dirname $0`/..
 git archive --format=zip --prefix=$NAME/ --output="$filename" master:$NAME
