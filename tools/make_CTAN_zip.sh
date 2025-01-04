c0e3e9de17ccc8af5fcfc93226c6a1b94ec201c0
Arthur Rosendahl <arthur.rosendahl@scilifelab.uu.se>
2025-01-05 00:52:17 +0100

    WIP Find the subtle mistake ...

--- tools/make_CTAN_zip.sh
+++ tools/make_CTAN_zip.sh
@@ -29,7 +29,7 @@ git push origin $release_branch
 ctan_root=$TMPDIR/$NAME/$NAME
 mkdir $ctan_root
 cd `dirname $0`/..
-rsync -avP $NAME/{README,VERSION} $ctan_root/
+rsync -avP $NAME/{README.md,VERSION} $ctan_root/
 git checkout $current_branch
 
 git archive --format=zip --prefix=$NAME/ --output="$tds_filename" $release_branch:$NAME
@@ -38,7 +38,10 @@ for topdir in tex source; do
   rsync -avP $NAME/$topdir/{generic,luatex}/$NAME/ $ctan_root/$topdir/
 done
 TMPDIR2=`mktemp -d /tmp/hyphXXXXXX`
+echo "CTAN-compliant directories:"
+pkgcheck -d $ctan_root
 unzip -d $TMPDIR2 $tds_filename
+echo "TDS zip:"
 pkgcheck -d $TMPDIR2/$NAME
 rm -rf $TMPDIR2
 echo "$tds_filename ready to be shipped to CTAN, matching contents of branch $release_branch."
